9213af8ad8d6a7e7656b54025d35b3a219256a4a
ivan770 <ivan@ivan770.me>
2025-02-02 08:27:11 -0500

    Add wasm-bindgen 0.2.100

--- src/wasm-bindgen.nix
+++ src/wasm-bindgen.nix
@@ -29,6 +29,10 @@
     wasm-bindgen-cli.override {
       inherit version hash cargoHash;
     }) {
+    "0.2.100" = {
+      hash = "sha256-3RJzK7mkYFrs7C/WkhW9Rr4LdP5ofb2FdYGz1P7Uxog=";
+      cargoHash = "sha256-tD0OY2PounRqsRiFh8Js5nyknQ809ZcHMvCOLrvYHRE=";
+    };
     "0.2.92" = {
       hash = "sha256-1VwY8vQy7soKEgbki4LD+v259751kKxSxmo/gqE6yV0=";
       cargoHash = "sha256-aACJ+lYNEU8FFBs158G1/JG8sc6Rq080PeKCMnwdpH0=";
